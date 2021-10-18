b234a57eadc25cc7492553a4d63071eb1a058fe9
TjandraD <tjandradarmo@protonmail.com>
2021-10-18 21:36:01 +0700

    feat: update checkbox value & title text decoration

--- lib/screens/widgets/task_tile.dart
+++ lib/screens/widgets/task_tile.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return CheckboxListTile(
-      title: const Text(
+      title: Text(
         "This is the first task",
         style: TextStyle(
           fontWeight: FontWeight.w600,
+          decoration: isChecked ? TextDecoration.lineThrough : null,
         ),
       ),
       controlAffinity: ListTileControlAffinity.trailing,
-      value: false,
-      onChanged: (value) {},
+      value: isChecked,
+      onChanged: (value) {
+        setState(() {
+          isChecked = !isChecked;
+        });
+      },
     );
   }
 }
