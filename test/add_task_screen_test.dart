91532b26697f28dc8f640d57284f199193a4a1ae
TjandraD <tjandradarmo@protonmail.com>
2021-10-20 20:34:54 +0700

    test: Add test for TextField in AddTaskScreen

--- test/add_task_screen_test.dart
+++ test/add_task_screen_test.dart
@@ -13,6 +13,19 @@ void main() {
       expect(find.text("Add Task"), findsOneWidget);
     });
 
+    testWidgets('TextField Should Change When Typed',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const app.MyApp());
+
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+
+      await tester.enterText(find.byType(TextField), "New Task Title");
+      await tester.pumpAndSettle();
+
+      expect(find.text("New Task Title"), findsOneWidget);
+    });
+
     testWidgets('MaterialButton Should Call onChanged When Tapped',
         (WidgetTester tester) async {
       await tester.pumpWidget(const app.MyApp());
@@ -23,7 +36,7 @@ void main() {
       await tester.tap(find.byType(MaterialButton));
       await tester.pumpAndSettle();
 
-      expect(find.byType(MaterialButton), findsOneWidget);
+      expect(find.byType(MaterialButton), findsNothing);
     });
   });
 }
